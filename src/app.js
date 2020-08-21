36e7b602f54bcfb55f9e801384f6c967f03ea37e
Alexander Georgiev <alexfromsofia@gmail.com>
2020-08-21 23:53:14 +0300

    Fix

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ require('./db/mongoose');
 
 require('dotenv').config();
 
-// const middlewares = require('./middlewares');
+const middlewares = require('./middlewares');
 const api = require('./api');
 
 const app = express();
